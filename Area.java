af1e3bbdf9f9d5b5841ffd6a1be0107fd098de22
plane29 <44649458+plane29@users.noreply.github.com>
2018-12-02 18:19:58 -0500

    Add files via upload

--- Area.java
+++ Area.java
@@ -14,13 +14,20 @@ public class Area extends JPanel{
 	public static final int BOX_HEIGHT = 640;
     public Image image;
     public static Graphics g;
-    public Rectangle rect;
+    public Rectangle rect[];
     public Area leftNeighbor;
     public Area rightNeighbor;
+    public Area forwardNeighbor;
+    public Area backwardNeighbor;
     public Area(Graphics g, String filename){
     	this.setPreferredSize(new Dimension(BOX_WIDTH, BOX_HEIGHT));
     	image = loadArea(g, filename);
-    	rect = new Rectangle(550, 300, 100, 75);
+    	rect = new Rectangle[2];
+    	rect[0] = new Rectangle(0,0, 100, BOX_HEIGHT);
+    	rect[1] = new Rectangle(860,0,100,640);
+    	//rect[2] = new Rectangle()
+    	//rect[3] =
+    	//rect[2] = new Rectangle(550, 300, 100, 75);
     }
 
     public Image loadArea(Graphics g, String filename){
@@ -34,20 +41,21 @@ public class Area extends JPanel{
     	return image;
     }
 
-    public void drawArea(Graphics g, Image img, int width, int height){
-    	g.drawImage(img,0,0,width,height,null);
+    public void drawArea(Graphics g){
+    	//g.drawRect( 10, 10, 100, 100);
+    	g.drawImage(image,0,0,BOX_WIDTH,BOX_HEIGHT,null);
     }
 
     @Override    
     public void paintComponent(Graphics g) {
         super.paintComponent(g);  
-        drawArea(g, image, BOX_WIDTH, BOX_HEIGHT);
-        rect.rectangleDraw(g);
+        drawArea(g);
+        //rect.rectangleDraw(g);
     }
 
     public static void main(String args[]){
     	JFrame frame = new JFrame("DrawToScreen");
-        Area area1 = new Area(g,"conference.jpg");
+        Area area1 = new Area(g,"kitchen.jpg");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(area1);
         frame.pack();
@@ -55,12 +63,20 @@ public class Area extends JPanel{
     }
 
     public void setRightNeighbor(Area neighbor){
-    	rightNeighbor = neighbor;
+    	this.rightNeighbor = neighbor;
     }
 
     public void setLeftNeighbor(Area neighbor){
-    	leftNeighbor = neighbor;
+    	this.leftNeighbor = neighbor;
     }
-    
+
+    public void setForwardNeighbor(Area neighbor){
+    	forwardNeighbor = neighbor;
+    }
+
+    public void setBackwardNeighbor(Area neighbor){
+    	backwardNeighbor = neighbor;
+    }
+
 }
 
